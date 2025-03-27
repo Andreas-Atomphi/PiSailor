657c111712a70be086dc9506939c5916b2843dc9
andreaseusoulin <andreaseusoulin@gmail.com>
2025-03-27 02:37:44 +0000

    [FEAT] Added StringUtils namespace

---
+++ src/lib/classes/helpers/string-utils.ts
@@ -0,0 +1,14 @@
+
+
+
+export namespace StringUtils {
+
+    export function format(str: string, ...val: string[]) {
+        for (let index = 0; index < val.length; index++) {
+            str = str.replace(`{${index}}`, val[index]);
+        }
+        return str;
+    }
+
+
+}
