b14904d91eb6f96ae8f6cf7147d8cfd99dda2149
andreaseusoulin <andreaseusoulin@gmail.com>
2025-11-23 20:35:58 +0000

    Added rule for warnings to ignore unused selector

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,19 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import adapter from '@sveltejs/adapter-auto';
 
 export default {
   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
   // for more information about preprocessors
   preprocess: vitePreprocess(),
+  kit: {
+		adapter: adapter()
+	},
+	onwarn: (warning, handler) => {
+		// Ignore "css_unused_selector" warnings
+		if (warning.code === 'css_unused_selector') {
+			return;
+		}
+		// Let other warnings be handled by the default handler
+		handler(warning);
+	}
 }
