68e7f4640635a7e15a15a937ca6910592f337ee4
andreaseusoulin <andreaseusoulin@gmail.com>
2025-11-23 20:41:26 +0000

    [FEAT] Make simple draggable menu for toggling and sorting filters order

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
       import "pixi.js/math-extras";
       import { onMount } from "svelte";
-      import { Graphics, Point } from "pixi.js";
+      import { Filter, Graphics, Point } from "pixi.js";
       import { Viewport } from "pixi-viewport";
       import hotkeys from "hotkeys-js";
+      import Sortable from 'sortablejs';
       import { setup } from "./app-sailor-setup";
       import Renderer from "./lib/components/Renderer.svelte";
       import {
@@ -15,13 +16,15 @@
       import { PiImageEncoder } from "./lib/classes/pi-image-encoder";
       import { Settings } from "./lib/classes/settings";
       import TriggerButton from "./lib/components/TriggerButton.svelte";
-      import Menu from "./lib/components/menu/Menu.svelte";
       import KeyIndicator from "./lib/components/KeyIndicator.svelte";
+      import { pixiShaders } from "./lib/classes/pixi-filters";
 
       let renderer: Renderer;
-      let menu: Menu;
+      let refreshShaders: () => void
+      let activeShaders = new Map(pixiShaders.entries().map(shader => [shader[0], false]));
 
       hotkeys.setScope("general");
+      let sortableList: HTMLUListElement;
 
       onMount(async () => {
             const { app, Input } = await setup(renderer!.getCanvas());
@@ -39,6 +42,17 @@
                   .pinch()
                   .wheel({ smooth: 3 });
             app.stage.addChild(viewport);
+            refreshShaders = () => {
+                  const filters: Filter[] = []
+                  for (const [name, enabled] of activeShaders.entries()) {
+                        if (!enabled) continue;
+                        if (activeShaders.get(name)) {
+                              filters.push(pixiShaders.get(name)!)
+                        }
+                  }
+                  app.stage.filters = filters;
+            }
+            app.stage.filters = []
 
             const piDigitsManager = new PiDigitsManager();
             const piImageEncoder = new PiImageEncoder();
@@ -109,78 +123,171 @@
             Input.addInputListener((event) => {
                   if (Input.isKeyJustPressed("KeyR")) viewport.setZoom(3, true);
             });
+
+            new Sortable(sortableList!, {
+                  animation: 150,
+                  onEnd: (evt) => {
+                        activeShaders = new Map();
+                        for (const li of sortableList!.children) {
+                              const checkbox = li.firstElementChild! as HTMLInputElement;
+                              activeShaders.set(checkbox.getAttribute("name")!, checkbox.checked);
+                        }
+                        refreshShaders();
+                  }
+            });
       });
 </script>
 
 <Renderer bind:this={renderer}></Renderer>
 
-<div class="
-      absolute
-      bottom-0
-      left-0
-      grid
-      grid-rows-2
-      grid-cols-4
-      justify-between
-      gap-4
-      bg-black
-      pt-4
-      pr-4
-      pb-10
-      pl-10
-      rounded-tr-xl
-      border-t-4
-      border-r-4
-      border-white
+<TriggerButton
+      className="
+            absolute
+            top-0
+            right-0
+      "
+      icon="menu"
+      hotkey="m"
+      onTrigger={() => {
+            document.getElementById("menu")!.classList.toggle("menu-hide");
+            document.getElementById("menu")!.classList.toggle("menu-show");
+      }}
+/>
+<div id="menu" class="absolute h-full top-0 w-100 pointer-events-none menu-hide">
+      <div class="
+            absolute
+            top-0
+            left-0
+            w-50
+            justify-between
+            bg-black
+            rounded-br-xl
+            border-b-4
+            border-r-4
+            border-white
+            pointer-events-auto
       ">
-            
-            <KeyIndicator
-                  key="q"
-                  icon="zoom_out"
-                  className="relative"
-                  shape="square"
-            />
-            <KeyIndicator
-                  key="w"
-                  icon="keyboard_arrow_up"
-                  className="relative"
-                  shape="square"
-            />
-            <KeyIndicator
-                  key="e"
-                  icon="zoom_in"
-                  className="relative"
-                  shape="square"
-            />
-            <KeyIndicator
-                  key="r"
-                  icon="settings_backup_restore"
-                  className="relative"
-                  shape="square"
-            />
-            <KeyIndicator
-                  key="a"
-                  icon="keyboard_arrow_left"
-                  className="relative"
-                  shape="square"
-            />
-            <KeyIndicator
-                  key="s"
-                  icon="keyboard_arrow_down"
-                  className="relative"
-                  shape="square"
-            />
-            <KeyIndicator
-                  key="d"
-                  icon="keyboard_arrow_right"
-                  className="relative"
-                  shape="square"
-            />
-            <KeyIndicator
-                  className="relative"
-                  shape="circle"
-            />
+            <ul bind:this={sortableList} id="sortable-list">
+                  {#each pixiShaders as shader}
+                        <li
+                              class="
+                                    text-white
+                                    flex
+                                    items-center
+                                    justify-between
+                                    pl-2
+                                    pr-2
+                                    border-b-1
+                                    border-t-1
+                                    border-white
+                              "
+                        >
+                              <input
+                                    id="checkbox-{shader[0]}"
+                                    name="{shader[0]}" type="checkbox"
+                                    class="border border-default-medium"
+                                    onchange={(e) => {
+                                          activeShaders.set(
+                                                shader[0],
+                                                e.currentTarget.checked
+                                          )
+                                          refreshShaders();
+                                    }}
+                              />
+                              <label
+                                    for="checkbox-{shader[0]}"
+                                    class="
+                                          select-none
+                                          w-full
+                                          py-4
+                                          ms-2
+                                          text-sm
+                                          font-medium
+                                          text-heading"
+                              >
+                                    {shader[0]}
+                              </label>
+                              <svg class="cursor-grab shrink-0 size-4 ms-auto text-white h-full" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                                    <circle cx="9" cy="12" r="1"></circle>
+                                    <circle cx="9" cy="5" r="1"></circle>
+                                    <circle cx="9" cy="19" r="1"></circle>
+                                    <circle cx="15" cy="12" r="1"></circle>
+                                    <circle cx="15" cy="5" r="1"></circle>
+                                    <circle cx="15" cy="19" r="1"></circle>
+                              </svg>
+                        </li>            
+                  {/each}
+            </ul>
+      </div>
 
+      <div class="
+            absolute
+            bottom-0
+            left-0
+            grid
+            grid-rows-2
+            grid-cols-4
+            justify-between
+            gap-4
+            bg-black
+            pt-4
+            pr-4
+            pb-10
+            pl-10
+            rounded-tr-xl
+            border-t-4
+            border-r-4
+            border-white
+            ">
+                  
+                  <KeyIndicator
+                        key="q"
+                        icon="zoom_out"
+                        className="relative"
+                        shape="square"
+                  />
+                  <KeyIndicator
+                        key="w"
+                        icon="keyboard_arrow_up"
+                        className="relative"
+                        shape="square"
+                  />
+                  <KeyIndicator
+                        key="e"
+                        icon="zoom_in"
+                        className="relative"
+                        shape="square"
+                  />
+                  <KeyIndicator
+                        key="r"
+                        icon="settings_backup_restore"
+                        className="relative"
+                        shape="square"
+                  />
+                  <KeyIndicator
+                        key="a"
+                        icon="keyboard_arrow_left"
+                        className="relative"
+                        shape="square"
+                  />
+                  <KeyIndicator
+                        key="s"
+                        icon="keyboard_arrow_down"
+                        className="relative"
+                        shape="square"
+                  />
+                  <KeyIndicator
+                        key="d"
+                        icon="keyboard_arrow_right"
+                        className="relative"
+                        shape="square"
+                  />
+                  <KeyIndicator
+                        className="relative"
+                        shape="circle"
+                  />
+
+      </div>
 </div>
 
 <style>
@@ -189,5 +296,20 @@
             padding: 0;
             background-color: black;
             overflow: hidden;
+      }
+
+      #menu {
+            transition: transform 0.25s cubic-bezier(.4, 0, .2, 1), opacity 0.25s cubic-bezier(.4, 0, .2, 1);
+      }
+
+      /* svelte-ignore unused-selector */
+      #menu.menu-show {
+            transform: translateX(0%);
+            opacity: 1.0;
+      }
+
+      #menu.menu-hide {
+            transform: translateX(-100%);
+            opacity: 0.0;
       }
 </style>
